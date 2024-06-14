c526b00fb459209fdfdc9beb4745f346aa88ac47
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-06-14 22:20:38 +0530

    index page

--- Application/views.py
+++ Application/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello')
+    return render(request, 'index.html')
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES['notesFile']:
+        noteFile = request.FILES['notesFile']
+        
+        return HttpResponse(request, noteFile)
